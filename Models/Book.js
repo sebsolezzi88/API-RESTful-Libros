61ea4cc6f100233b51358cd3ecd7443f4976411d
sebsolezzi88 <ssolezzi1@gmail.com>
2025-06-24 15:23:52 -0300

    Creacion de los modelos

---
+++ Models/Book.js
@@ -0,0 +1,53 @@
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../config/database.js');
+const User = require('./User');
+
+class Book extends Model {}
+
+Book.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    bookname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.ENUM('unread', 'reading', 'read'),
+      allowNull: false,
+      defaultValue: 'unread',
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+      validate: {
+        min: 0,
+        max: 10,
+      },
+    },
+    userId: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Book',
+    tableName: 'books',
+    timestamps: true,
+  }
+);
+
+// Relaciones
+Book.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+User.hasMany(Book, { foreignKey: 'userId', as: 'books' });
+
+module.exports = Book;
---
+++ Models/User.js
@@ -0,0 +1,36 @@
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../config/database.js');
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM('admin', 'user'),
+      allowNull: false,
+      defaultValue: 'user',
+    },
+  },
+  {
+    sequelize,
+    modelName: 'User',
+    tableName: 'users',
+    timestamps: false,
+  }
+);
+
+module.exports = User;
