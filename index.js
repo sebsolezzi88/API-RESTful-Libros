0e0fb89ebcf66bb4e533770ea559f092966a628d
sebsolezzi88 <ssolezzi1@gmail.com>
2025-06-24 15:19:05 -0300

    Agregando las rutas

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import authRoutes from './routes/authRoutes.js';
 
 //Cargar variables de entorno
 dotenv.config();
@@ -8,6 +9,9 @@ const PORT = process.env.PORT || 3200;
 const app = express();
 
 app.use(express.json());
+
+//Rutas
+app.use('/auth',authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
