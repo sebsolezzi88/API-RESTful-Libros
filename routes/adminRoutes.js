1ad25e4d1fd3823dcc46fb6c4ae9bad53c9dd5b3
sebsolezzi88 <ssolezzi1@gmail.com>
2025-06-25 10:11:39 -0300

    Añadiendo middelwarea a rutas de admin

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,12 +2,13 @@ import { Router } from 'express';
 
 import { verifyToken } from '../middlewares/authMiddlewares.js';
 import { deleteAnyUser, getAllBooks } from '../controllers/adminController.js';
+import { isAdmin } from '../middlewares/adminMiddelwares.js';
 
 
 const router = Router();
 
-router.get('/getallbooks',verifyToken,getAllBooks);
-router.delete('/addbook',verifyToken ,deleteAnyUser);
+router.get('/getallbooks',verifyToken,isAdmin,getAllBooks);
+router.delete('/addbook',verifyToken ,isAdmin,deleteAnyUser);
 
 
 
