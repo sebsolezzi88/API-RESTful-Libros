d5e0d6bf00f7558a9f17d241fc153f2ca44212e9
sebsolezzi88 <ssolezzi1@gmail.com>
2025-06-25 10:14:32 -0300

    Funcion para que el admin vea todos los libros de los usuarios

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,26 @@
-export const getAllBooks = async (req , res )=>{
+import Book from '../Models/Book.js'
 
+//Obtener todos los libros
+export const getAllBooks = async (req , res )=>{
+    try {
+        const books = await Book.findAll();
+        
+        if (books.length === 0) {
+            return res.json({
+                status: 'success',
+                message: 'No books found',
+                books: []
+            });
+        }
+
+        return res.json({
+            status: 'success',
+            message: 'Books retrieved successfully',
+            books
+        });
+    } catch (error) {
+          return res.status(500).json({status:'error', message:'internal server error',error});
+    }
 }
 
 export const deleteAnyUser = async (req , res )=>{
